82c6e75e8cd69104fd4d06efa15bf6707b33cc33
SlayerOrnstein <HydroNav@protonmail.com>
2021-10-05 01:53:31 -0400

    test: app test now test for bazarpage instead of the counter page

--- test/app_test.dart
+++ test/app_test.dart
@@ -5,15 +5,42 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:bazar_repository/bazar_repository.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
 import 'package:market/app/app.dart';
-import 'package:market/counter/counter.dart';
+import 'package:market/bazar/view/bazar_page.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
+  late BazarCache cache;
+  late BazarRepository repository;
+
+  setUp(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    if (!kIsWeb) {
+      final tempPath = await getTemporaryDirectory();
+
+      Hive.init(tempPath.path);
+    }
+
+    cache = BazarCache(temp: await Hive.openBox<dynamic>('bazar_cache'));
+    repository = BazarRepository(cache: cache);
+  });
+
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+    testWidgets('renders BazarPage', (tester) async {
+      await tester.pumpWidget(
+        RepositoryProvider.value(
+          value: repository,
+          child: const App(),
+        ),
+      );
+      expect(find.byType(BazarPage), findsOneWidget);
     });
   });
 }
